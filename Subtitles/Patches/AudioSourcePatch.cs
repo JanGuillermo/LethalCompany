23a253d5d24d6feedbe155f24eb18684e43330bc
Jan <6843446+JanGuillermo@users.noreply.github.com>
2023-12-10 13:18:35 -0800

    Add AudibleDistanceLib dependency on Subtitles and update to 2.1.0 (#30)
    * Add AUdibleDistanceLib
    
    * Use AudibleDistanceLib in Subtitles
    
    * Add AudibleDistanceLib dependendy on Subtitles and update to 2.1.0

--- Subtitles/Patches/AudioSourcePatch.cs
+++ Subtitles/Patches/AudioSourcePatch.cs
@@ -1,9 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using GameNetcodeStuff;
 using HarmonyLib;
 using UnityEngine;
+using static AudibleDistanceLib.AudibleDistanceLib;
 using static SubtitlesAPI.SubtitlesAPI;
 
 namespace Subtitles.Patches;
@@ -25,7 +25,7 @@ public class AudioSourcePatch
 
         AudioSource source = gameObject.GetComponent<AudioSource>();
 
-        if (IsInWithinAudiableDisable(source, volume))
+        if (IsInWithinAudiableDistance(GameNetworkManager.Instance, source, volume, Plugin.Instance.minimumAudibleVolume.Value))
         {
             AddSubtitle(clip);
         }
@@ -37,7 +37,7 @@ public class AudioSourcePatch
     [HarmonyPatch(nameof(AudioSource.PlayOneShotHelper), new[] { typeof(AudioSource), typeof(AudioClip), typeof(float) })]
     public static void PlayOneShotHelper_Prefix(AudioSource source, ref AudioClip clip, float volumeScale)
     {
-        if (IsInWithinAudiableDisable(source, volumeScale))
+        if (IsInWithinAudiableDistance(GameNetworkManager.Instance, source, volumeScale, Plugin.Instance.minimumAudibleVolume.Value))
         {
             AddSubtitle(clip);
         }
@@ -48,7 +48,7 @@ public class AudioSourcePatch
     public static void PlayDelayed_Prefix(AudioSource __instance)
     {
         if (__instance.clip == null) return;
-        if (IsInWithinAudiableDisable(__instance, __instance.volume))
+        if (IsInWithinAudiableDistance(GameNetworkManager.Instance, __instance, __instance.volume, Plugin.Instance.minimumAudibleVolume.Value))
         {
             AddSubtitle(__instance.clip);
         }
@@ -59,7 +59,7 @@ public class AudioSourcePatch
     public static void Play_Prefix(AudioSource __instance)
     {
         if (__instance.clip == null) return;
-        if (IsInWithinAudiableDisable(__instance, __instance.volume))
+        if (IsInWithinAudiableDistance(GameNetworkManager.Instance, __instance, __instance.volume, Plugin.Instance.minimumAudibleVolume.Value))
         {
             AddSubtitle(__instance.clip);
         }
@@ -122,61 +122,5 @@ public class AudioSourcePatch
         }
 
         return $"<color=green>{translation}</color>";
-    }
-
-    private static bool IsInWithinAudiableDisable(AudioSource source, float volume)
-    {
-        if (volume == 0 || source is null || GameNetworkManager.Instance?.localPlayerController is null)
-        {
-            return false;
-        }
-
-        bool isPlayerDead = GameNetworkManager.Instance.localPlayerController.isPlayerDead;
-        bool isSpeculating = ((Object)(object)GameNetworkManager.Instance.localPlayerController.spectatedPlayerScript != null);
-        PlayerControllerB playerController = (!isPlayerDead || !isSpeculating) ? GameNetworkManager.Instance.localPlayerController : GameNetworkManager.Instance.localPlayerController.spectatedPlayerScript;
-
-        float distance = Vector3.Distance(playerController.transform.position, source.transform.position);
-        float audibleVolume = EvaluateVolumeAt(source, distance) * volume;
-
-        return audibleVolume >= (Plugin.Instance.minimumAudibleVolume.Value / 100);
-    }
-
-    private static float EvaluateVolumeAt(AudioSource source, float distance)
-    {
-        AnimationCurve curve = null;
-        float range = source.maxDistance - source.minDistance;
-
-        if (distance < source.minDistance)
-        {
-            return 1;
-        }
-
-        if (distance > source.maxDistance)
-        {
-            return 0;
-        }
-
-        switch (source.rolloffMode)
-        {
-            case AudioRolloffMode.Linear:
-                curve = AnimationCurve.Linear(0, 1, 1, 0);
-                break;
-            case AudioRolloffMode.Logarithmic:
-                // My best guess at what the logarithmic curve is
-                curve = new(new(0, 1), new(range / 4, 1 / (source.minDistance + range / 4)), new(range / 2, 1 / (source.minDistance + range / 2)), new(3 * range / 4, 1 / (source.minDistance + 3 * range / 4)), new(1, 0));
-                break;
-            case AudioRolloffMode.Custom:
-                curve = source.GetCustomCurve(AudioSourceCurveType.CustomRolloff);
-                break;
-        }
-
-        if (curve is null)
-        {
-            return 1;
-        }
-
-        float evalutationDistance = (distance - source.minDistance) / range;
-
-        return curve.Evaluate(evalutationDistance);
     }
 }
